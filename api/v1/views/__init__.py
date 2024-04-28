716dd1b959f3c89ddbb3542d4ab583ffec525fd9
proabudu <abuduprosperyao@gmail.com>
2024-04-28 07:59:59 +0000

    module import correction

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,9 @@
 """Init file for views module"""
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
-from api.v1.views.index import *
-from api.v1.views.states import *
+# Imports from subfolders go here
+from . import views.index  # Import from views.index subfolder
+from . import views.states  # Import from views.states subfolder
