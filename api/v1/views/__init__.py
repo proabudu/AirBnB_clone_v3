51a790a8f0d7fe837a3a0cd854b743f8a818fb81
proabudu <abuduprosperyao@gmail.com>
2024-04-28 08:29:57 +0000

    trial to sort init file issue

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Init file for views module"""
-from Flask import Blueprint
+
+# Init file for views module
+
+from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
