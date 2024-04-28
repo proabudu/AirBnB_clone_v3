e3e41f6ae51cbe88a24e50b98f086f697345e792
proabudu <abuduprosperyao@gmail.com>
2024-04-28 07:07:41 +0000

    update to index.py file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This module contains endpoint(route) status
 """
@@ -12,21 +11,17 @@ from flask import jsonify
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-
     """
     Returns a JSON status
     """
-
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def count():
-
     """
     Retrieves the number of each objects by type
     """
-    
     return jsonify({"amenities": storage.count("Amenity"),
                     "cities": storage.count("City"),
                     "places": storage.count("Place"),
